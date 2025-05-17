ebc3bd2670358a5c3ea7770fca8651fbd1376272
Thusikaran <pushparasa.thusikaran@gmail.com>
2025-05-17 13:39:03 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,45 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Particles from './component/Particles'
+import GooeyNav from './component/GooeyNav'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const items = [
+  { label: "Home", href: "#" },
+  { label: "About", href: "#" },
+  { label: "Contact", href: "#" },
+];
+
   return (
     <>
-        Thusikaran love Vanuja
+
+    <Particles
+    particleColors={['#ffffff', '#ffffff']}
+    particleCount={200}
+    particleSpread={10}
+    speed={0.1}
+    particleBaseSize={100}
+    moveParticlesOnHover={true}
+    alphaParticles={false}
+    disableRotation={false}
+  />
+       <div className='Navbar'>
+          <div style={{ height: '600px', position: 'relative' }}>
+            <GooeyNav
+              items={items}
+              particleCount={15}
+              particleDistances={[90, 10]}
+              particleR={100}
+              initialActiveIndex={0}
+              animationTime={600}
+              timeVariance={300}
+              colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+            />
+          </div>
+       </div>
     </>
   )
 }
